4f1bc17287a8da86e06e1c0e8828eb2217b0dec6
Giorgio <imatesiu@gmail.com>
2016-11-17 20:34:44 +0100

    onActivityResult added

--- app/src/main/java/it/isti/sse/provehwmf/AllegatoActivity.java
+++ app/src/main/java/it/isti/sse/provehwmf/AllegatoActivity.java
@@ -1,13 +1,16 @@
 package it.isti.sse.provehwmf;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.github.clans.fab.FloatingActionMenu;
 
@@ -16,6 +19,7 @@ import java.io.File;
 import it.isti.sse.provehwmf.util.Utility;
 
 public class AllegatoActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,12 +79,52 @@ public class AllegatoActivity extends AppCompatActivity {
                         .setAction("Action", null).show();*/
                 FloatingActionMenu menuRed = (FloatingActionMenu) findViewById(R.id.menuAHW);
                 menuRed.close(false);
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");      //all files
+                //intent.setType("text/xml");   //XML file only
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                try {
+                    startActivityForResult(Intent.createChooser(intent, "Select a File to Upload"), 1);
+
+                } catch (android.content.ActivityNotFoundException ex) {
+                    // Potentially direct the user to the Market with a Dialog
+                    Toast.makeText(view.getContext(), "Please install a File Manager.", Toast.LENGTH_SHORT).show();
+                }
                 // Intent i = new Intent(ProvaActivity.this, NoteActivity.class);
                 // i.putExtra("key","value");
                 // startActivity(i);
             }
         });
 
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                String result=data.getStringExtra("result");
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.allegatoactivity);
+                Snackbar.make(coordinatorLayout, "Nessun allegato selezionato", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        }
+        if (requestCode == 100) {
+            if(resultCode == Activity.RESULT_OK){
+                String result=data.getStringExtra("result");
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.allegatoactivity);
+                Snackbar.make(coordinatorLayout, "Nessun allegato selezionato", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 }
