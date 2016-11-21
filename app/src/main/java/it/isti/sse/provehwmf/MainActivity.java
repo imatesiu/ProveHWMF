bf14b2aef93f9b8dba4b093272d98491a006f4c4
Giorgio <imatesiu@gmail.com>
2016-11-21 19:06:50 +0100

    request permission added

--- app/src/main/java/it/isti/sse/provehwmf/MainActivity.java
+++ app/src/main/java/it/isti/sse/provehwmf/MainActivity.java
@@ -1,10 +1,14 @@
 package it.isti.sse.provehwmf;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -49,7 +53,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        requestPermission();
         FloatingActionMenu menuRed = (FloatingActionMenu) findViewById(R.id.menu);
         menuRed.setClosedOnTouchOutside(true);
 
@@ -220,6 +224,47 @@ public class MainActivity extends AppCompatActivity
                     .setAction("Action", null).show();
         }
         return null;
+    }
+
+    private void requestPermission(){
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA,Manifest.permission.INTERNET
+                            ,Manifest.permission.READ_EXTERNAL_STORAGE
+                            ,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.ACCESS_FINE_LOCATION},
+                    900);
+        }
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 900: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted
+
+                } else {
+                    //TODO:close app
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @Override
