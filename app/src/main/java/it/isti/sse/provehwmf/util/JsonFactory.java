3f5c4bafdff2bddc4551c64d36d145b06bbc23a3
Giorgio <imatesiu@gmail.com>
2016-11-21 23:46:00 +0100

    factory added

--- app/src/main/java/it/isti/sse/provehwmf/util/JsonFactory.java
+++ app/src/main/java/it/isti/sse/provehwmf/util/JsonFactory.java
@@ -24,8 +24,64 @@ public class JsonFactory {
     }
 
     private void init() {
+        Allegato a2 = new Allegato();
+        a2.setMatricola("1122334455");
+        a2.setNome("Foto00000000000000000000000000000000.jpg");
+        a2.setTipo("JPG");
+        a2.setUserid("Gio");
+        a2.setTime("12/12/2016 12:15:16");
+
+        Allegato a1 = new Allegato();
+        a1.setMatricola("1122334455");
+        a1.setNome("Foto00000000000000000000000000000000.jpg");
+        a1.setTipo("JPG");
+        a1.setUserid("Gio");
+        a1.setTime("12/12/2016 12:15:16");
+
+
+        Allegati aa1 = new Allegati();
+        aa1.getAllegato().add(a2);
+        aa1.getAllegato().add(a1);
+
+
+        ProvaHW phw2 = new ProvaHW();
+        phw2.setEsito(Esito.Positivo);
+        phw2.setMatricola("1122334455");
+        phw2.setTipo(TipoProve.DisturbiElettromagnetici.toString());
+        phw2.setModello("TIPO C");
+        phw2.setUserid("Gio");
+        phw2.setTimeStartPHW("12/12/2016 12:15:16");
+        phw2.setTimeEndPHW("13/12/2016 12:15:16");
+        phw2.setAllegati(aa1);
+        phw2.setNote(new Note());
+
+        ProvaHW phw1 = new ProvaHW();
+        phw1.setEsito(Esito.Positivo);
+        phw1.setMatricola("1122334455");
+        phw1.setTipo(TipoProve.DisturbiElettromagnetici.toString());
+        phw1.setModello("TIPO C");
+        phw1.setUserid("Gio");
+        phw1.setTimeStartPHW("12/12/2016 12:15:16");
+        phw1.setTimeEndPHW("13/12/2016 12:15:16");
+        phw1.setAllegati(aa1);
+        phw1.setNote(new Note());
+
+        ProveHW LPHW1 = new ProveHW();
+        LPHW1.getProvaHW().add(phw2);
+        LPHW1.getProvaHW().add(phw1);
+
+        MisuratoreFiscale MF = new MisuratoreFiscale();
+        MF.setDitta("SHS");
+        MF.setModello("TIPO C");
+        MF.setMatricola("1122334455");
+        MF.setTimeMFStart("12/12/2016 12:15:16");
+        MF.setTimeMFEnd("13/12/2016 12:15:16");
+        MF.setNome("TIPO");
+        MF.setProveHW(LPHW1);
+        MF.setNote(new Note());
+
         Allegato a = new Allegato();
-        a.setMatricola("1122334455");
+        a.setMatricola("112233445566");
         a.setNome("Foto00000000000000000000000000000000.jpg");
         a.setTipo("JPG");
         a.setUserid("Gio");
@@ -35,7 +91,7 @@ public class JsonFactory {
 
         ProvaHW phw = new ProvaHW();
         phw.setEsito(Esito.Positivo);
-        phw.setMatricola("1122334455");
+        phw.setMatricola("112233445566");
         phw.setTipo(TipoProve.DisturbiElettromagnetici.toString());
         phw.setModello("TIPO C");
         phw.setUserid("Gio");
@@ -47,19 +103,19 @@ public class JsonFactory {
         ProveHW LPHW = new ProveHW();
         LPHW.getProvaHW().add(phw);
 
-
-        MisuratoreFiscale MF = new MisuratoreFiscale();
-        MF.setDitta("SHS");
-        MF.setModello("TIPO C");
-        MF.setMatricola("1122334455");
-        MF.setTimeMFStart("12/12/2016 12:15:16");
-        MF.setTimeMFEnd("13/12/2016 12:15:16");
-        MF.setNome("TIPO");
-        MF.setProveHW(LPHW);
-        MF.setNote(new Note());
+        MisuratoreFiscale MF2 = new MisuratoreFiscale();
+        MF2.setDitta("SHS");
+        MF2.setModello("TIPO C");
+        MF2.setMatricola("112233445566");
+        MF2.setTimeMFStart("12/12/2016 12:15:16");
+        MF2.setTimeMFEnd("13/12/2016 12:15:16");
+        MF2.setNome("TIPO");
+        MF2.setProveHW(LPHW);
+        MF2.setNote(new Note());
 
         LMF = new MisuratoriFiscale();
         LMF.getMisuratoreFiscale().add(MF);
+        LMF.getMisuratoreFiscale().add(MF2);
 
     }
 
